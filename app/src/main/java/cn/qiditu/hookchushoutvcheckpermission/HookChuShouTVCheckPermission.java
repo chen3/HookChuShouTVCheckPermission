f0d4bf93ef71c6dd380bb7fb99d1e2528e401ecd
chen3 <laoc1995@163.com>
2018-07-15 18:11:23 +0800

    first commit

---
+++ app/src/main/java/cn/qiditu/hookchushoutvcheckpermission/HookChuShouTVCheckPermission.java
@@ -0,0 +1,18 @@
+package cn.qiditu.hookchushoutvcheckpermission;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by on 2018-07-15.
+ */
+
+public class HookChuShouTVCheckPermission implements IXposedHookLoadPackage {
+    /**
+     * {innerDoc}
+     */
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    }
+}
